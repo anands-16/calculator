4876668fa6b0678a87e7656b56fba3094d67469d
anandsebastian <anandsebastian16@gmail.com>
2021-01-03 17:29:44 +0530

    title and userdata taken

---
+++ calculator.py
@@ -0,0 +1,11 @@
+from tkinter import *
+root = Tk()
+#Title for calc
+root.title("Scientific Calculator")
+
+#Taking user input
+user_input = Entry(root,width =30,borderwidth=5,relief=RIDGE)
+
+
+
+root.mainloop()
